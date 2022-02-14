f57b187785f08889f974285dddd5749c862b41b2
gmclonex <firek7731@gmail.com>
2022-02-14 11:34:10 +1000

    Add LUNA rewards to weBENT claim page

--- src/constant/pool.ts
+++ src/constant/pool.ts
@@ -296,7 +296,7 @@ const BentCvxStaking = {
 
 const weBENT = {
 	Addr: '0x04637d61F538911929ff96E755B589C014fD9ce2',
-	RewardAssets: ['BENTCVX', 'SPELL', 'FXS', 'CVX']
+	RewardAssets: ['BENTCVX', 'SPELL', 'FXS', 'CVX', 'LUNAWORM']
 }
 
 export const POOLS = {
